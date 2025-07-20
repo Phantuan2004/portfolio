0dc7548bb73e92e1d0065fd6670a3321e5d4b52a
phantuan2004 <phanhoangtuan21072004@gmail.com>
2025-07-20 14:58:30 +0700

    create data

---
+++ src/data/project.js
@@ -0,0 +1,35 @@
+const projects = [
+    {
+        img: "assets/images/project/project-1.jpg",
+        title: "Project Heading",
+        desc: "Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ",
+        link: "project.html",
+        client: "Google",
+    },
+
+    {
+        img: "assets/images/project/project-2.jpg",
+        title: "Project Heading",
+        desc: "Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ",
+        link: "project.html",
+        client: "Dropbox",
+    },
+
+    {
+        img: "assets/images/project/project-3.jpg",
+        title: "Project Heading",
+        desc: "Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ",
+        link: "project.html",
+        client: "Amazon",
+    },
+
+    {
+        img: "assets/images/project/project-4.jpg",
+        title: "Project Heading",
+        desc: "Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ",
+        link: "project.html",
+        client: "Facebook",
+    }
+]
+
+export default projects;
---
+++ src/data/skills.js
@@ -0,0 +1,51 @@
+const skills = [
+    {
+        icon: "fa-brands fa-js-square",
+        title: "Vanilla JavaScript",
+        desc: "Basic",
+    },
+
+    {
+        icon: ["fa-brands fa-angular me-2", "fa-brands fa-react", "fa-brands fa-vuejs"],
+        title: "Angular, React &amp; Vue",
+        desc: "Basic",
+    },
+
+    {
+        icon: "fa-brands fa-node-js",
+        title: "Node.js",
+        desc: "Basic",
+    },
+
+    {
+        icon: ["fa-brands fa-python"],
+        title: "Python &amp; Django",
+        desc: "Basic",
+    },
+
+    {
+        icon: "fa-brands fa-php",
+        title: "PHP",
+        desc: "Basic",
+    },
+
+    {
+        icon: ["fa-brands fa-npm me-2", "fa-brands fa-gulp me-2", "fa-brands fa-grunt"],
+        title: "npm, Gulp &amp; Grunt",
+        desc: "Basic",
+    },
+
+    {
+        icon: ["fa-brands fa-html5 me-2", "fa-brands fa-css3-alt"],
+        title: "HTML &amp; CSS",
+        desc: "Basic",
+    },
+
+    {
+        icon: ["fa-brands fa-sass me-2", "fa-brands fa-less"],
+        title: "Sass &amp; LESS",
+        desc: "Basic",
+    }
+]
+
+export default skills;
