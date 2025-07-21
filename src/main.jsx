bb1aac0164cc1d022d3fa16f251b9534217e24c1
phantuan2004 <phanhoangtuan21072004@gmail.com>
2025-07-21 18:05:52 +0700

    add feateru language on library i18next

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+// Import library i18next
+import "./library/i18next/i18n.js";
 
 // Import Font Awesome CSS
 import "@fortawesome/fontawesome-free/css/all.min.css";
 // // Import Bootstrap CSS
 // import "bootstrap/dist/css/bootstrap.min.css";
-// // Import Bootstrap JS
-// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// Import Bootstrap JS
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
